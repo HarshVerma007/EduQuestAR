d2cc68c397075868a59f67203a79fce6ee15f8d3
sukritihere <sukritisingh9469@gmail.com>
2023-11-30 00:14:25 +0530

    3d model

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { BrowserRouter } from "react-router-dom";
 
-import { About, Contact, Experience, Feedbacks, Hero, Navbar, Tech, Works, StarsCanvas } from "./components";
+import { About, Experience, Feedbacks, Hero, Navbar, Tech, Works, StarsCanvas } from "./components";
 
 const App = () => {
   return (
@@ -17,7 +17,7 @@ const App = () => {
         <Works />
         <Feedbacks />
         <div className='relative z-0'>
-          <Contact />
+         
           <StarsCanvas />
         </div>
       </div>
