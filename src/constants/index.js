b377e945ca398da64a58a082ff88160f617eece5
sukritihere <sukritisingh9469@gmail.com>
2023-11-30 17:33:43 +0530

    skeleton apk file

--- src/constants/index.js
+++ src/constants/index.js
@@ -238,7 +238,7 @@ const projects = [
       // },
     ],
     image: skeleton1,
-    source_code_link: "https://github.com/",
+    source_code_link: "src/assets/skeleton.apk",
   },
   {
     name: " Human Heart",
@@ -259,7 +259,7 @@ const projects = [
       // },
     ],
     image: heart,
-    source_code_link: "https://github.com/",
+    source_code_link: "src/assets/heart.apk",
   },
   {
     name: "Solar Sytsem",
@@ -280,7 +280,7 @@ const projects = [
       // },
     ],
     image: solar,
-    source_code_link: "https://github.com/",
+    source_code_link: "src/assets/SOLARSYSTEM.apk",
   },
   {
     name: "Ar Shooter",
